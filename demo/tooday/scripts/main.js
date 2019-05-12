b1a0f587fc93a8eae880cf2c5a9f3e3fab4ea023
raziEiL <mr.raz4291@gmail.com>
2019-05-12 16:07:35 +0500

    Added gamburger menu

--- demo/tooday/scripts/main.js
+++ demo/tooday/scripts/main.js
@@ -75,6 +75,25 @@ function linkOnClick(navItem, section) {
     selectedSection = section;
 }
 
+const MENU = document.getElementsByClassName("nav__menu")[0];
+const NAV_LIST = document.getElementsByClassName("nav__list")[0];
+const MENU_ACTIVE = "nav__menu-active";
+const NAV_ACTIVE = "nav__list-active";
+// Toggle class to elem
+MENU.addEventListener("click", function() {
+    ToggleClass(this, MENU_ACTIVE);
+    ToggleClass(NAV_LIST, NAV_ACTIVE);
+});
+
+function ToggleClass(elem, toggle) {
+    console.log(MENU);
+    if (elem.className.search(toggle) != -1) {
+        elem.className = elem.className.split(toggle).join('').trim();
+    }
+    else {
+        elem.className += " " + toggle;
+    }
+}
 /* LINK_ARRAY[0].addEventListener("click", function () {
     console.log(1, this); // elem
 });
