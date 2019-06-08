40ebae6f107138355957f15eb451e7e3447c4e90
raziEiL <mr.raz4291@gmail.com>
2019-06-08 07:39:49 +0500

    Masonry layout, color vars, improve text, fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,11 @@ function ToggleClass(elem, toggle) {
         elem.className += " " + toggle;
     }
 }
-
+// Использование "Кирпичной кладки": добавить класс masonry-grid к контейнеру, а класс masonry-item к дочерним элементам
 // https://codepen.io/razieil/pen/arPVmX
-var hiddenDiv = document.getElementById("advantages__item-fake");
-const ITEM_ARRAY = document.getElementsByClassName("advantages__item");
-const GRID = document.getElementsByClassName("advantages__grid")[0];
+var hiddenDiv = document.getElementById("masonry-item-fake");
+const ITEM_ARRAY = document.getElementsByClassName("masonry-item");
+const GRID_ARRAY = document.getElementsByClassName("masonry-grid");
 var lineHeight;
 
 if (hiddenDiv != null) {
@@ -30,7 +30,7 @@ if (hiddenDiv != null) {
     alignGridRows();
 }
 else {
-    console.log("Error id=advantages__item-fake not found");
+    console.log("Error id=masonry-item-fake not found");
 }
 
 function onResize() {
@@ -41,9 +41,14 @@ function onResize() {
 function alignGridRows() {
     if (lineHeight === undefined)
         return;
-    GRID.style.alignItems = "start";
+
+    for (let i = 0; i < GRID_ARRAY.length; i++)
+        GRID_ARRAY[i].style.alignItems = "start";
+
     for (let i = 0; i < ITEM_ARRAY.length; i++) {
         ITEM_ARRAY[i].style.gridRow = "span " + Math.round(ITEM_ARRAY[i].offsetHeight / lineHeight);
     }
-    GRID.style.alignItems = null;
+
+    for (let i = 0; i < GRID_ARRAY.length; i++)
+        GRID_ARRAY[i].style.alignItems = null;
 }
