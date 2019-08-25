ddb4dcaf0fcff04c61670530e32e393890d4d290
Denis Medvedev <mr.raz4291@gmail.com>
2019-08-25 21:44:34 +0500

    More algorithm

--- practice/algorithm.js
+++ practice/algorithm.js
@@ -147,10 +147,104 @@ console.log(mutation(["floor", "for"])); // true
 // Разбить массив на группы по N элементов
 function chunkArrayInGroups(arr, size) {
     let newArr = [];
-    for (let i = 0; i < arr.length; i += size) 
+    for (let i = 0; i < arr.length; i += size)
         newArr.push(arr.slice(i, i + size));
     return newArr;
 }
 
 console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2)); // [ [ 'a', 'b' ], [ 'c', 'd' ] ]
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6 ] ]
+
+
+// Поиск различий в массивах
+function diffArray(arr1, arr2) {
+    let newArr = [];
+    let tempArr = [].concat(arr2);
+
+    arr1.forEach(x => {
+        if (tempArr.some(y => y == x))
+            tempArr.splice(tempArr.indexOf(x), 1);
+        else
+            newArr.push(x);
+    });
+    return newArr.concat(tempArr);
+}
+// [ 4 ]
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+// [ 'pink wool', 'diorite' ]
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+
+
+// Поиск и удаление значений из массива
+function destroyer(arr) {
+    //console.log(arguments.length);
+    let index;
+    for (let i = 1; i < arguments.length; i++) {
+        while ((index = arr.indexOf(arguments[i])) != -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
+}
+// [ 1, 1 ]
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+// Поиск и удаление значений из массива (без мутаций)
+function destroyerF(arr) {
+    // [ [ 1, 2, 3, 1, 2, 3 ], 2, 3 ] -> [ 2, 3 ]
+    let argsArr = Array.from(arguments).splice(1);
+    return arr.filter(x => !argsArr.includes(x));
+}
+// [ 1, 1 ]
+console.log(destroyerF([1, 2, 3, 1, 2, 3], 2, 3));
+
+
+// Поиск совпадений в объекте
+function whatIsInAName(collection, source) {
+    return collection.filter(obj => {
+        for (let x in source) {
+            if (!obj.hasOwnProperty(x) || obj[x] !== source[x])
+                return false;
+        }
+        return true;
+    });
+}
+
+// [ { apple: 1, bat: 2 }, { apple: 1, bat: 2, cookie: 2 } ]
+console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }));
+// [ { first: 'Tybalt', last: 'Capulet' } ]
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+
+// Spinal Tap Case
+function spinalCase(str) {
+    return str.trim().match(/[A-Z][a-z]+|[a-z]+/g).join("-").toLowerCase();
+}
+// this-is-spinal-tap
+console.log(spinalCase('This Is Spinal Tap'));
+// this-is-spinal-tap
+console.log(spinalCase('thisIsSpinalTap'));
+
+
+// Pig Latin
+function translatePigLatin(str) {
+    const reg = /^[^aeiou]+/i;
+    if (reg.test(str)) {
+        return str.replace(reg, "") + str.match(reg)[0] + "ay";
+    }
+    return str + "way";
+}
+// aliforniacay
+console.log(translatePigLatin("california"));
+//algorithmway
+console.log(translatePigLatin("algorithm"));
+
+
+// Поиск и замена (если before начинается с заглавной, то заглавная устанавливается и для after)
+function myReplace(str, before, after) {
+    let index = str.indexOf(before);
+    // "A quick brown fox " + "leaped" + " over the lazy dog"
+    return str.slice(0, index) + (/^[A-Z]/.test(before) ? (after.charAt(0).toUpperCase() + after.slice(1)) : after) + str.slice(index + before.length);
+}
+
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
