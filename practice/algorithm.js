68153531c50f66be378d0c37f867704701b41fcf
Denis Medvedev <mr.raz4291@gmail.com>
2019-08-28 18:34:42 +0500

    added better logs

--- practice/algorithm.js
+++ practice/algorithm.js
@@ -1,6 +1,6 @@
 // === Алгоритмы ===
 
-// Найти самое длинное слово в строке
+// #1 Найти самое длинное слово в строке
 function findLongestWordLength(str) {
     let arr = str.split(" ");
     let prevLen = 0;
@@ -14,10 +14,10 @@ function findLongestWordLength(str) {
     return word;
 }
 // jumped
-console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+log(1, findLongestWordLength("The quick brown fox jumped over the lazy dog"));
 
 
-// Найти наибольшее значение в многомерном массиве
+// #2 Найти наибольшее значение в многомерном массиве
 function largestOfFour(arr) {
     let prevNum;
     let array = [];
@@ -38,18 +38,18 @@ function largestOfFour(arr) {
     return array;
 }
 // [25, 48, 21, -3]
-console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
+log(2, largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
 
 
-// Подтвердить окончание
+// #3 Подтвердить окончание
 function confirmEnding(str, target) {
     return str.slice(str.length - target.length) === target;
 }
 // true
-console.log(confirmEnding("Bastian", "an"));
+log(3, confirmEnding("Bastian", "an"));
 
 
-// Повторить строку
+// #4 Повторить строку
 function repeatStringNumTimes(str, num) {
     let s = "";
     while (num > 0) {
@@ -59,10 +59,10 @@ function repeatStringNumTimes(str, num) {
     return s;
 }
 // abcabcabc
-console.log(repeatStringNumTimes("abc", 3));
+log(4, repeatStringNumTimes("abc", 3));
 
 
-// Обрезать строку
+// #5 Обрезать строку
 function truncateString(str, num) {
     // Clear out that junk in your trunk
     if (str.length > num)
@@ -70,10 +70,10 @@ function truncateString(str, num) {
     return str;
 }
 // A-tisket...
-console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+log(5, truncateString("A-tisket a-tasket A green and yellow basket", 8));
 
 
-// Найти первое значение из массива используя функцию как условие
+// #6 Найти первое значение из массива используя функцию как условие
 function findElement(arr, func) {
     for (let i of arr) {
         if (func(i))
@@ -82,10 +82,10 @@ function findElement(arr, func) {
     return undefined;
 }
 // 2
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+log(6, findElement([1, 2, 3, 4], num => num % 2 === 0));
 
 
-// Первая Буква Всегда Заглавная
+// #7 Первая Буква Всегда Заглавная
 /** @arg {string} str */
 function titleCase(str) {
     let arr = str.toLocaleLowerCase().split(" ");
@@ -95,10 +95,10 @@ function titleCase(str) {
     return arr.join(" ");
 }
 // Is'm A Little Tea Pot
-console.log(titleCase("iS'm a LITTLE tea pOt"));
+log(7, titleCase("iS'm a LITTLE tea pOt"));
 
 
-// Соединить массивы
+// #8 Соединить массивы
 function frankenSplice(arr1, arr2, n) {
     let arrCopy = arr2.slice();
     for (let i = 0; i < arr1.length; i++)
@@ -106,12 +106,12 @@ function frankenSplice(arr1, arr2, n) {
     return arrCopy;
 }
 // [4, 1, 2, 3, 5, 6]
-console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+log(8, frankenSplice([1, 2, 3], [4, 5, 6], 1));
 // [ 'head', 'shoulders', 'claw', 'tentacle', 'knees', 'toes' ]
-console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
+log(8, frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
 
 
-// Поместить число в массив учитывая его значения
+// #9 Поместить число в массив учитывая его значения
 function getIndexToIns(arr, num) {
     arr.sort((a, b) => a - b);
     let val = arr.length;
@@ -123,12 +123,13 @@ function getIndexToIns(arr, num) {
     }
     return val;
 }
+// 3
+log(9, getIndexToIns([10, 20, 30, 40, 50], 35));
+// 0
+log(9, getIndexToIns([3, 10, 5], 3));
 
-console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
-console.log(getIndexToIns([3, 10, 5], 3)); // 0
 
-
-// True если первая строка содержит все символы второй строки
+// #10 True если первая строка содержит все символы второй строки
 function mutation(arr) {
     let src = arr[1].toLowerCase();
     let str = arr[0].toLowerCase();
@@ -139,23 +140,24 @@ function mutation(arr) {
     return true;
 }
 
-console.log(mutation(["hello", "hey"])); // false
-console.log(mutation(["floor", "for"])); // true
+log(10, mutation(["hello", "hey"])); // false
+log(10, mutation(["floor", "for"])); // true
 
 
-// Разбить массив на группы по N элементов
+// #11 Разбить массив на группы по N элементов
 function chunkArrayInGroups(arr, size) {
     let newArr = [];
     for (let i = 0; i < arr.length; i += size)
         newArr.push(arr.slice(i, i + size));
     return newArr;
 }
+// [ [ 'a', 'b' ], [ 'c', 'd' ] ]
+log(11, chunkArrayInGroups(["a", "b", "c", "d"], 2));
+// [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6 ] ]
+log(11, chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2)); // [ [ 'a', 'b' ], [ 'c', 'd' ] ]
-console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6 ] ]
 
-
-// Поиск различий в массивах
+// #12 Поиск различий в массивах
 function diffArray(arr1, arr2) {
     let newArr = [];
     let tempArr = [].concat(arr2);
@@ -169,14 +171,14 @@ function diffArray(arr1, arr2) {
     return newArr.concat(tempArr);
 }
 // [ 4 ]
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+log(12, diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 // [ 'pink wool', 'diorite' ]
-console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+log(12, diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 
 
-// Поиск и удаление значений из массива
+// #13 Поиск и удаление значений из массива
 function destroyer(arr) {
-    //console.log(arguments.length);
+    //log(1, arguments.length);
     let index;
     for (let i = 1; i < arguments.length; i++) {
         while ((index = arr.indexOf(arguments[i])) != -1) {
@@ -186,19 +188,20 @@ function destroyer(arr) {
     return arr;
 }
 // [ 1, 1 ]
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+log(13, destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 
-// Поиск и удаление значений из массива (без мутаций)
+
+// #14 Поиск и удаление значений из массива (без мутаций)
 function destroyerF(arr) {
     // [ [ 1, 2, 3, 1, 2, 3 ], 2, 3 ] -> [ 2, 3 ]
     let argsArr = Array.from(arguments).splice(1);
     return arr.filter(x => !argsArr.includes(x));
 }
 // [ 1, 1 ]
-console.log(destroyerF([1, 2, 3, 1, 2, 3], 2, 3));
+log(14, destroyerF([1, 2, 3, 1, 2, 3], 2, 3));
 
 
-// Поиск совпадений в объекте
+// #15 Поиск совпадений в объекте
 function whatIsInAName(collection, source) {
     return collection.filter(obj => {
         for (let x in source) {
@@ -210,22 +213,22 @@ function whatIsInAName(collection, source) {
 }
 
 // [ { apple: 1, bat: 2 }, { apple: 1, bat: 2, cookie: 2 } ]
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }));
+log(15, whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }));
 // [ { first: 'Tybalt', last: 'Capulet' } ]
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+log(15, whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
 
 
-// Spinal Tap Case
+// #16 Spinal Tap Case
 function spinalCase(str) {
     return str.trim().match(/[A-Z][a-z]+|[a-z]+/g).join("-").toLowerCase();
 }
 // this-is-spinal-tap
-console.log(spinalCase('This Is Spinal Tap'));
+log(16, spinalCase('This Is Spinal Tap'));
 // this-is-spinal-tap
-console.log(spinalCase('thisIsSpinalTap'));
+log(16, spinalCase('thisIsSpinalTap'));
 
 
-// Pig Latin
+// #17 Pig Latin
 function translatePigLatin(str) {
     const reg = /^[^aeiou]+/i;
     if (reg.test(str)) {
@@ -234,22 +237,22 @@ function translatePigLatin(str) {
     return str + "way";
 }
 // aliforniacay
-console.log(translatePigLatin("california"));
+log(17, translatePigLatin("california"));
 // algorithmway
-console.log(translatePigLatin("algorithm"));
+log(17, translatePigLatin("algorithm"));
 
 
-// Поиск и замена (если before начинается с заглавной, то заглавная устанавливается и для after)
+// #18 Поиск и замена (если before начинается с заглавной, то заглавная устанавливается и для after)
 function myReplace(str, before, after) {
     let index = str.indexOf(before);
     // "A quick brown fox " + "leaped" + " over the lazy dog"
     return str.slice(0, index) + (/^[A-Z]/.test(before) ? (after.charAt(0).toUpperCase() + after.slice(1)) : after) + str.slice(index + before.length);
 }
+// A quick brown fox leaped over the lazy dog
+log(18, myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
 
-console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
 
-
-// Составить пару ДНК
+// #19 Составить пару ДНК
 function pairElement(str) {
     let arr = [];
 
@@ -271,10 +274,10 @@ function pairElement(str) {
     return arr;
 }
 // [ [ 'G', 'C' ], [ 'C', 'G' ], [ 'G', 'C' ] ]
-console.log(pairElement("GCG"));
+log(19, pairElement("GCG"));
 
 
-// Вернуть пропущенную букву
+// #20 Вернуть пропущенную букву
 function fearNotLetter(str) {
     let validLetter;
     for (let i = 1; i < str.length; i++) {
@@ -285,12 +288,12 @@ function fearNotLetter(str) {
     return undefined;
 }
 // d
-console.log(fearNotLetter("abce"));
+log(20, fearNotLetter("abce"));
 // undefined
-console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
+log(20, fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
 
 
-// Выборка уникальных значений из массивов
+// #21 Выборка уникальных значений из массивов
 function uniteUnique() {
     let arr = [];
     for (let arg of arguments) {
@@ -302,12 +305,12 @@ function uniteUnique() {
     return arr;
 }
 // [ 1, 3, 2, 5, 4 ]
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+log(21, uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
 // [ 1, 3, 2, [ 5 ], [ 4 ] ]
-console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]));
+log(21, uniteUnique([1, 3, 2], [1, [5]], [2, [4]]));
 
 
-// Замена символов html кодами
+// #22 Замена символов html кодами
 function convertHTML(str) {
     const symbols = {
         "&": "&amp;",
@@ -320,12 +323,12 @@ function convertHTML(str) {
     return str.split("").map(x => symbols[x] || x).join("");
 }
 // Dolce &amp; Gabbana
-console.log(convertHTML("Dolce & Gabbana"));
+log(22, convertHTML("Dolce & Gabbana"));
 // Hamburgers &lt; Pizza &lt; Tacos
-console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+log(22, convertHTML("Hamburgers < Pizza < Tacos"));
 
 
-// Сумма нечетных чисел Фибоначчи до заданного числа (включительно)
+// #23 Сумма нечетных чисел Фибоначчи до заданного числа (включительно)
 function sumFibs(num) {
     let sum = 0;
     let prevNum = 0;
@@ -340,10 +343,10 @@ function sumFibs(num) {
     return sum;
 }
 // 5
-console.log(sumFibs(4));
+log(23, sumFibs(4));
 
 
-// Сумма простых чисел до заданного числа (включительно)
+// #24 Сумма простых чисел до заданного числа (включительно)
 function sumPrimes(num) {
     let sum = 0;
     for (let i = 2; i <= num; i++) {
@@ -364,10 +367,10 @@ function sumPrimes(num) {
     return sum;
 }
 // 17
-console.log(sumPrimes(10));
+log(24, sumPrimes(10));
 
 
-// Найти наименьшее общее кратное с учетом диапазона чисел
+// #25 Найти наименьшее общее кратное с учетом диапазона чисел
 function smallestCommons(arr) {
     let range = arr.sort((a, b) => a - b);
     const lastNum = range.pop();
@@ -384,6 +387,70 @@ function smallestCommons(arr) {
     return mult;
 }
 // 6
-console.log(smallestCommons([3, 1]));
+log(25, smallestCommons([3, 1]));
 // 12
-console.log(smallestCommons([2, 4]));
+log(25, smallestCommons([2, 4]));
+
+
+// #26 Удаляет первый элемент в массиве пока условие не будет соблюдено
+function dropElements(arr, func) {
+    const LEN = arr.length;
+    for (let i = 0; i < LEN; i++) {
+        if (func(arr[0]))
+            break;
+        else
+            arr.shift();
+    }
+    return arr;
+}
+// [ 3, 4 ]
+log(26, dropElements([1, 2, 3, 4], function(n) { return n > 5; }));
+
+
+// #27 Развернуть массив любого уровня 
+function steamrollArray(arr) {
+    let steamrRoller = [];
+    loop(arr);
+
+    function loop(arr) {
+        for (let i of arr) {
+            if (Array.isArray(i))
+                loop(i);
+            else
+                steamrRoller.push(i);
+        }
+    }
+    return steamrRoller;
+}
+// [ 1, 2, 3, 4 ]
+log(27, steamrollArray([1, [2], [3, [[4]]]]));
+
+
+// #28 Binary to String
+function binaryAgent(str) {
+    return String.fromCharCode(...str.split(" ").map(x => parseInt(x, 2)));
+}
+// Aren't bonfires fun!?
+log(28, binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+
+// #29 Возвращает true если значение свойства объекта truthy https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+function truthCheck(collection, pre) {
+    return collection.every(obj => {
+        for (let i in obj) {
+            if (i == pre)
+                return obj[i]; // truthy
+        }
+        return false;
+        // 2 вариант
+        // return obj.hasOwnProperty(pre) && Boolean(obj[pre]);
+    })
+}
+// true
+log(29, truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex"));
+// false
+log(29, truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Po", "sex": 0 }], "sex"));
+
+function log(num, result) {
+    console.log(num + ")", result);
+}
